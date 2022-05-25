e53c7b842688c86b2d001d45b13634a43ed2fbe4
Jason Whyttes <jwhyttes@gmail.com>
2022-05-26 02:27:13 +1000

    Update to work with mesh selections and transforms

--- bbox_to_ctrl.py
+++ bbox_to_ctrl.py
@@ -2,15 +2,20 @@
 +-------------------------------------------------------------+
 | simple tool to create a curve out of a objects bounding box |
 |-------------------------------------------------------------|
-| 1)    Select your object(s).                                |
+| 1)    Select your object.                                   |
 | 2)    Run the script.                                       |
 +-------------------------------------------------------------+
 """
 import maya.cmds as cmds
-sel = cmds.ls(sl=True)
-for i in range(0, len(sel)):
+
+def bbox_ctrl():
+    sel = cmds.ls(sl=True,fl=True)
+    if cmds.objectType(sel[0]) == 'mesh':
+        bbox_name = sel[0].split('.')[0]
+    else:
+        bbox_name = sel[0]
     try:
-        bbox = cmds.exactWorldBoundingBox(sel[i])
+        bbox = cmds.exactWorldBoundingBox(sel)
         crvData = { "points":[(bbox[0],bbox[4],bbox[2]),
         (bbox[3],bbox[4],bbox[2]),
         (bbox[3],bbox[1],bbox[2]),
@@ -28,7 +33,8 @@ for i in range(0, len(sel)):
         (bbox[0],bbox[1],bbox[5]),
         (bbox[0],bbox[1],bbox[2])],
         "knots":range(0,16)}
-        bbox_crv = cmds.curve(n = '{}_bbox_ctrl'.format(sel[i]), d=1,p=crvData['points'],k=crvData['knots'])
+        bbox_crv = cmds.curve(n = '{}_bbox_ctrl'.format(bbox_name), d=1,p=crvData['points'],k=crvData['knots'])
         cmds.select(cl=1)
     except TypeError:
         pass
+bbox_ctrl()
